9f5c68f34615d57ffff7b5453c0a224da7797fc7
CrIsTiLaz <cristilazea18@gmail.com>
2023-01-14 23:47:46 +0200

    add movie covers

--- src/App.js
+++ src/App.js
@@ -29,6 +29,7 @@ function App() {
       movie={movie}/>))
       
   )
+  
   console.log('movies', movies)
 
   return (
--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,9 +1,14 @@
 import React from 'react'
 
 const MovieCard = ({ movie }) => {
+  const IMAGE_PATH = "https://image.tmdb.org/t/p/w500"
+  console.log(movie)
   return (
-    <div>
-      {movie.title}
+    <div className={"movie-card"}>
+      {movie.poster_path ? <img className={'movie-cover'} src={`${IMAGE_PATH}${movie.poster_path}`} alt=""/>
+      : null
+    }
+      <h5 className={'movie-title'}>{movie.title}</h5>
     </div>
 
   )
